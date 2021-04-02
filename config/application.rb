89b1fb559859a4171077b40e7866adfc1a85a294
Richard Xia <richardxia@richardxia.com>
2021-04-01 23:49:27 -0700

    Remove everything related to assets pipeline.

--- config/application.rb
+++ config/application.rb
@@ -3,7 +3,6 @@
 require File.expand_path('boot', __dir__)
 
 require 'rails/all'
-require 'uglifier'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
